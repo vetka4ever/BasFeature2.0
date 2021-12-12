df25904d2b6d618fd482d42b3cacafedb5aec545
vetka4ever <dansh@sfedu.ru>
2021-12-12 17:08:44 +0300

    test commit

--- BasketApp2.0/AppDelegate.swift
+++ BasketApp2.0/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = true
         window = UIWindow.init(frame: UIScreen.main.bounds)
